c6096d7066c30ae8233a4239db00e0f01a17b76d
Penguinnke <penguinnke2801@gmail.com>
2023-02-19 16:38:12 +0100

    Trying to fix controls, not working yet

--- BPW_2/Assets/Character/Character.cs
+++ BPW_2/Assets/Character/Character.cs
@@ -8,6 +8,7 @@ public class Character : MonoBehaviour
     private CharacterController characterController;
 
     public float Speed = 5f;
+    public float CameraSpeed;
 
     void Start()
     {
@@ -19,6 +20,11 @@ public class Character : MonoBehaviour
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         characterController.Move(move * Time.deltaTime * Speed);
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.eulerAngles += CameraSpeed * new Vector3(x:0, y:Input.GetAxis("Mouse X"), z:0);
+        }
     }
 
 }
