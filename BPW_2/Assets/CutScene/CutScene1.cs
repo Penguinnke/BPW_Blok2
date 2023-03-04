fb06f425fc87c0fdfa0bf2e63d94411feccc64d1
Penguinnke <penguinnke2801@gmail.com>
2023-03-04 23:03:58 +0100

    Update CutScene1.cs

--- BPW_2/Assets/CutScene/CutScene1.cs
+++ BPW_2/Assets/CutScene/CutScene1.cs
@@ -12,9 +12,12 @@ public class CutScene1 : MonoBehaviour
     public GameObject PurpleBlock;
 
 
-    void Start() 
+    void Update() 
     {
+        if (Input.GetKey("w"))
+        {
         StartCoroutine("SecondsWait");
+        }
     }
 
     private IEnumerator SecondsWait() //makes it wait a couple of seconds before the healthbooster is seen again
@@ -23,16 +26,10 @@ public class CutScene1 : MonoBehaviour
             //The little white square, which is the healthbooster, should dissapear right after the player touched it
             RedBlock.GetComponent<Image>().enabled = false;
 
-            StartCoroutine("SecondsWait");
+           // StartCoroutine("SecondsWait");
 
             yield return new WaitForSeconds(5);
             Debug.Log("Coroutine activated");
-
-            // Healthbooster.GetComponent<BoxCollider2D>().enabled = true;
-            // Healthbooster.GetComponent<Image>().enabled = true;
-            // Healthbooster_TEXT.enabled = true;
-
-            // SpawnOnANewPlace();
     }
 
 }
